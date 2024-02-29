df5f0bb4c9a8b529225eb3475fec31ed6daf7fb7
Lanxin Zheng <lzhe3378@uni.sydney.edu.au>
2024-02-29 20:33:07 +1100

    final update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const weatherRouter=require('./weaterRouter')
 //create a web server
 const app= express();
+app.use(express.json())
 const PORT=8000;
 app.use(weatherRouter);
 app.listen(PORT,()=>{
--- weaterRouter.js
+++ weaterRouter.js
@@ -34,11 +34,60 @@ let data = [
     res.send(foundItems);
   })
   //POST add a new item
-
+weatherRouter.post('/activities',(req,res)=>{
+  console.log('body',req.body)
+  const{activity,weather}=req.body;
+  if(!activity||!weather){
+    return res.status(400).send('activity and weather are required')
+  }
+  data.push({activity,weather});
+  res.status(201).send({msg:'add activity succeeded'})
+})
   //GET activties based on weather condition /:condition/rainy
-
+weatherRouter.get('/activities/weather/:condition',(req,res)=>{
+  const weatherCondition =req.params.condition;
+  console.log(weatherCondition)
+  if(!weatherCondition){
+    res.status(400).send('weather condition is required')
+  }
+  const itemsWithCondition=data.filter(item=>{
+    return item.weather===weatherCondition;
+  })
+  res.send(itemsWithCondition)
+})
   //PUT update activities :/condition 
-
+weatherRouter.put('/activities/weather/:condition',(req,res)=>{
+  const weatherCondition=req.params.condition;
+  const {activity}=req.body;
+  const foundIndex =data.findIndex(item =>{
+    return item.weather ===weatherCondition;
+  })
+  if(foundIndex ===-1){
+    return res.status(404).send('weather condition not found')
+  }
+  data[foundIndex].activity=activity;
+  res.send({
+    msg:'act update successed',
+    updateWeather:data[foundIndex]
+  })
+})
   //DELETE items based on weathear condition /:condition/rainy
+weatherRouter.delete('/activities/weather/:condition',(req,res)=>{
+  console.log(req.params);
+  const weatherCondition=req.params.condition;
+  
+  const foundIndex =data.findIndex(item =>{
+    return item.weather ===weatherCondition;
+  });
+  if(foundIndex ===-1){
+    return res.status(404).send('weather condition not found')
+  }
+  const deleteWeather =data.splice(foundIndex,1);
+  res.send({
+    msg:"act is delete successfully",
+    //deleteWeather:deleteWeather(0)
+  })
 
+
+})
   module.exports=weatherRouter;
